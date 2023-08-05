61f454d7dd180e02d91dbe097e440d2e171017ef
dengjinlong <dengjinlong@jtexpress.com>
2023-08-05 16:02:00 +0800

    add android method

--- android/src/main/java/com/txplayer/TxplayerView.java
+++ android/src/main/java/com/txplayer/TxplayerView.java
@@ -12,119 +12,164 @@ import com.tencent.liteav.demo.superplayer.SuperPlayerDef;
 import com.tencent.liteav.demo.superplayer.SuperPlayerView;
 
 
-/**
- * feed流需求的播放器控件
- */
 public class TxplayerView extends FrameLayout {
 
-    private SuperPlayerView superPlayerView        = null;
-    private FeedPlayerCallBack feedPlayerCallBack     = null;
+  private SuperPlayerView superPlayerView        = null;
+  private FeedPlayerCallBack feedPlayerCallBack     = null;
 
-    private int                position               = -1;
-    private boolean            playWithModelIsSuccess = false;
+  private int                position               = -1;
+  private boolean            playWithModelIsSuccess = false;
 
-    public TxplayerView(@NonNull Context context) {
-        super(context);
-        this.initViews();
-    }
+  private String videoURL;
+  private String appId;
+  private String fileId;
+  private String psign;
+  private Boolean enableSlider;
+  private Boolean enableMorePanel;
+  private Boolean enableDownload;
+  private Boolean enableDanmaku;
+  private Boolean enableFullScreen;
+  private Integer playType;
+  private Float playStartTime;
+  private String language;
+  public void setVideoURL(String videoURL) {
+    this.videoURL = videoURL;
+  }
+  public void setAppId(String appId) {
+    this.appId = appId;
+  }
+  public void setFileId(String fileId) {
+    this.fileId = fileId;
+  }
+  public void setPsign(String psign) {
+    this.psign = psign;
+  }
+  public void setEnableSlider(Boolean enableSlider) {
+    this.enableSlider = enableSlider;
+  }
+  public void setEnableMorePanel(Boolean enableMorePanel) {
+    this.enableMorePanel = enableMorePanel;
+  }
+  public void setEnableDownload(Boolean enableDownload) {
+    this.enableDownload = enableDownload;
+  }
+  public void setEnableDanmaku(Boolean enableDanmaku) {
+    this.enableDanmaku = enableDanmaku;
+  }
+  public void setEnableFullScreen(Boolean enableFullScreen) {
+    this.enableFullScreen = enableFullScreen;
+  }
+  public void setPlayType(Integer playType) {
+    this.playType = playType;
+  }
+  public void setPlayStartTime(Float playStartTime) {
+    this.playStartTime = playStartTime;
+  }
+  public void setLanguage(String language) {
+    this.language = language;
+  }
 
-    public TxplayerView(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        this.initViews();
-    }
+  public TxplayerView(@NonNull Context context) {
+    super(context);
+    this.initViews();
+  }
+  public TxplayerView(@NonNull Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+    this.initViews();
+  }
 
-    public TxplayerView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        this.initViews();
-    }
+  public TxplayerView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    this.initViews();
+  }
 
-    private void initViews() {
-        superPlayerView = new SuperPlayerView(getContext());
-        superPlayerView.showOrHideBackBtn(false);
-        superPlayerView.setPlayerViewCallback(new SuperPlayerView.OnSuperPlayerViewCallback() {
-            @Override
-            public void onStartFullScreenPlay() {
-                if (feedPlayerCallBack != null) {
-                    feedPlayerCallBack.onStartFullScreenPlay();
-                }
-            }
+  private void initViews() {
+    superPlayerView = new SuperPlayerView(getContext());
+    superPlayerView.showOrHideBackBtn(false);
+    superPlayerView.setPlayerViewCallback(new SuperPlayerView.OnSuperPlayerViewCallback() {
+      @Override
+      public void onStartFullScreenPlay() {
+        if (feedPlayerCallBack != null) {
+          feedPlayerCallBack.onStartFullScreenPlay();
+        }
+      }
 
-            @Override
-            public void onStopFullScreenPlay() {
-                if (feedPlayerCallBack != null) {
-                    feedPlayerCallBack.onStopFullScreenPlay();
-                }
-            }
+      @Override
+      public void onStopFullScreenPlay() {
+        if (feedPlayerCallBack != null) {
+          feedPlayerCallBack.onStopFullScreenPlay();
+        }
+      }
 
-            @Override
-            public void onClickFloatCloseBtn() {
+      @Override
+      public void onClickFloatCloseBtn() {
 
-            }
+      }
 
-            @Override
-            public void onClickSmallReturnBtn() {
-                if (feedPlayerCallBack != null) {
-                    feedPlayerCallBack.onClickSmallReturnBtn();
-                }
-            }
+      @Override
+      public void onClickSmallReturnBtn() {
+        if (feedPlayerCallBack != null) {
+          feedPlayerCallBack.onClickSmallReturnBtn();
+        }
+      }
 
-            @Override
-            public void onStartFloatWindowPlay() {
+      @Override
+      public void onStartFloatWindowPlay() {
 
-            }
+      }
 
-            @Override
-            public void onPlaying() {
+      @Override
+      public void onPlaying() {
 //                if (feedPlayerManager != null) {
 //                    feedPlayerManager.setPlayingFeedPlayerView(FeedPlayerView.this, position);
 //                }
-                // 开始播放后，重置播放开始时间
-                superPlayerView.setStartTime(0);
-            }
+        // 开始播放后，重置播放开始时间
+        superPlayerView.setStartTime(0);
+      }
 
-            @Override
-            public void onPlayEnd() {
+      @Override
+      public void onPlayEnd() {
 
-            }
+      }
 
-            @Override
-            public void onError(int code) {
+      @Override
+      public void onError(int code) {
 //                if (SuperPlayerCode.VOD_REQUEST_FILE_ID_FAIL == code) {
 //                }
 //                if (feedPlayerManager != null) {
 //                    feedPlayerManager.removePlayingFeedPlayerView(position);
 //                }
-            }
+      }
 
-            @Override
-            public void onShowCacheListClick() {
+      @Override
+      public void onShowCacheListClick() {
 
-            }
-        });
-        addView(superPlayerView);
-    }
+      }
+    });
+    addView(superPlayerView);
+  }
 
-    /**
-     * 设置播放器管理类
-     *
-     * @param manager
-     */
+  /**
+   * 设置播放器管理类
+   *
+   * @param manager
+   */
 //    public void setFeedPlayerManager(FeedPlayerManager manager) {
 //        feedPlayerManager = manager;
 //    }
 
-    /**
-     * 设置回调接口
-     *
-     * @param callBack
-     */
-    public void setFeedPlayerCallBack(FeedPlayerCallBack callBack) {
-        feedPlayerCallBack = callBack;
-    }
+  /**
+   * 设置回调接口
+   *
+   * @param callBack
+   */
+  public void setFeedPlayerCallBack(FeedPlayerCallBack callBack) {
+    feedPlayerCallBack = callBack;
+  }
 
-    public FeedPlayerCallBack getFeedPlayerCallBack() {
-        return feedPlayerCallBack;
-    }
+  public FeedPlayerCallBack getFeedPlayerCallBack() {
+    return feedPlayerCallBack;
+  }
 
 
 //    public void preparePlayVideo(int position, VideoModel videoModel) {
@@ -159,84 +204,84 @@ public class TxplayerView extends FrameLayout {
 //        }
 //    }
 
-    public void resume() {
-        if (superPlayerView != null) {
-            if (playWithModelIsSuccess) {
-                superPlayerView.onResume();
-            } else {
+  public void resume() {
+    if (superPlayerView != null) {
+      if (playWithModelIsSuccess) {
+        superPlayerView.onResume();
+      } else {
 //                play(videoModel);
-            }
-        }
+      }
     }
+  }
 
-    public void pause() {
-        if (superPlayerView != null) {
-            superPlayerView.onPause();
-        }
+  public void pause() {
+    if (superPlayerView != null) {
+      superPlayerView.onPause();
     }
+  }
 
-    public void stop() {
-        if (superPlayerView != null) {
-            position = -1;
-            superPlayerView.stopPlay();
-        }
+  public void stop() {
+    if (superPlayerView != null) {
+      position = -1;
+      superPlayerView.stopPlay();
     }
+  }
 
-    public void reset() {
-        position = -1;
-        if (superPlayerView != null) {
-            superPlayerView.revertUI();
-        }
+  public void reset() {
+    position = -1;
+    if (superPlayerView != null) {
+      superPlayerView.revertUI();
     }
+  }
 
-    public void destroy() {
-        reset();
-        if (superPlayerView != null) {
-            superPlayerView.setPlayerViewCallback(null);
-            superPlayerView.resetPlayer();
-            superPlayerView.release();
-        }
-        superPlayerView = null;
+  public void destroy() {
+    reset();
+    if (superPlayerView != null) {
+      superPlayerView.setPlayerViewCallback(null);
+      superPlayerView.resetPlayer();
+      superPlayerView.release();
     }
+    superPlayerView = null;
+  }
 
-    public boolean isPlaying() {
-        return superPlayerView.getPlayerState() == SuperPlayerDef.PlayerState.PLAYING;
-    }
+  public boolean isPlaying() {
+    return superPlayerView.getPlayerState() == SuperPlayerDef.PlayerState.PLAYING;
+  }
 
-    public boolean isFullScreenPlay() {
-        return superPlayerView.getPlayerMode() == SuperPlayerDef.PlayerMode.FULLSCREEN;
-    }
+  public boolean isFullScreenPlay() {
+    return superPlayerView.getPlayerMode() == SuperPlayerDef.PlayerMode.FULLSCREEN;
+  }
 
-    public void setWindowPlayMode() {
-        superPlayerView.switchPlayMode(SuperPlayerDef.PlayerMode.WINDOW);
-    }
+  public void setWindowPlayMode() {
+    superPlayerView.switchPlayMode(SuperPlayerDef.PlayerMode.WINDOW);
+  }
 
-    public boolean isEnd() {
-        return superPlayerView.getPlayerState() == SuperPlayerDef.PlayerState.END;
-    }
+  public boolean isEnd() {
+    return superPlayerView.getPlayerState() == SuperPlayerDef.PlayerState.END;
+  }
 
-    public int getPosition() {
-        return position;
-    }
+  public int getPosition() {
+    return position;
+  }
 
-    public interface FeedPlayerCallBack {
-        void onStartFullScreenPlay();
+  public interface FeedPlayerCallBack {
+    void onStartFullScreenPlay();
 
-        void onStopFullScreenPlay();
+    void onStopFullScreenPlay();
 
-        void onClickSmallReturnBtn();
+    void onClickSmallReturnBtn();
 
-    }
+  }
 
-    public void onRequestPermissionsResult(int requestCode, @NonNull int[] grantResults) {
-        superPlayerView.onRequestPermissionsResult(requestCode,grantResults);
-    }
+  public void onRequestPermissionsResult(int requestCode, @NonNull int[] grantResults) {
+    superPlayerView.onRequestPermissionsResult(requestCode,grantResults);
+  }
 
-    public void setStartTime(int progress) {
-        superPlayerView.setStartTime(progress);
-    }
+  public void setStartTime(int progress) {
+    superPlayerView.setStartTime(progress);
+  }
 
-    public long getProgress() {
-        return superPlayerView.getProgress();
-    }
+  public long getProgress() {
+    return superPlayerView.getProgress();
+  }
 }
--- android/src/main/java/com/txplayer/TxplayerViewManager.java
+++ android/src/main/java/com/txplayer/TxplayerViewManager.java
@@ -4,13 +4,22 @@ import android.graphics.Color;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 
-public class TxplayerViewManager extends SimpleViewManager<View> {
+import java.util.Map;
+
+public class TxplayerViewManager extends SimpleViewManager<TxplayerView> {
   public static final String REACT_CLASS = "TxplayerView";
+
+  public final int COMMAND_STARTPLAY = 1;
+  public final int COMMAND_STOPPLAY = 2;
+  public final int COMMAND_ADDDanmuk = 3;
 
   @Override
   @NonNull
@@ -20,12 +29,87 @@ public class TxplayerViewManager extends SimpleViewManager<View> {
 
   @Override
   @NonNull
-  public View createViewInstance(ThemedReactContext reactContext) {
-    return new View(reactContext);
+  public TxplayerView createViewInstance(ThemedReactContext reactContext) {
+    return new TxplayerView(reactContext);
   }
 
-  @ReactProp(name = "color")
-  public void setColor(View view, String color) {
-    view.setBackgroundColor(Color.parseColor(color));
+  @Nullable
+  @Override
+  public Map<String, Integer> getCommandsMap() {
+    return MapBuilder.of(
+      "startPlay", COMMAND_STARTPLAY,
+      "stopPlay", COMMAND_STOPPLAY,
+      "addDanmaku", COMMAND_ADDDanmuk
+      );
+  }
+
+  @Override
+  public void receiveCommand(
+    @NonNull TxplayerView root,
+    String commandId,
+    @Nullable ReadableArray args
+  ) {
+    super.receiveCommand(root, commandId, args);
+    int reactNativeViewId = args.getInt(0);
+    int commandIdInt = Integer.parseInt(commandId);
+
+    switch (commandIdInt) {
+      case COMMAND_STARTPLAY:
+        break;
+      case COMMAND_STOPPLAY:
+        break;
+      case COMMAND_ADDDanmuk:
+        break;
+      default: {}
+    }
+  }
+
+  @ReactProp(name = "videoURL")
+  public void setVideoURL(TxplayerView view, String videoURL) {
+    view.setVideoURL(videoURL);
+  }
+  @ReactProp(name = "appId")
+  public void setAppId(TxplayerView view, String appId) {
+    view.setAppId(appId);
+  }
+  @ReactProp(name = "fileId")
+  public void setFileId(TxplayerView view, String fileId) {
+    view.setFileId(fileId);
+  }
+  @ReactProp(name = "psign")
+  public void setPsign(TxplayerView view, String psign) {
+    view.setPsign(psign);
+  }
+  @ReactProp(name = "enableSlider")
+  public void setEnableSlider(TxplayerView view, String enableSlider) {
+    view.setEnableSlider(Boolean.valueOf(enableSlider));
+  }
+  @ReactProp(name = "enableMorePanel")
+  public void setEnableMorePanel(TxplayerView view, String enableMorePanel) {
+    view.setEnableMorePanel(Boolean.valueOf(enableMorePanel));
+  }
+  @ReactProp(name = "enableDownload")
+  public void setEnableDownload(TxplayerView view, String enableDownload) {
+    view.setEnableDownload(Boolean.valueOf(enableDownload));
+  }
+  @ReactProp(name = "enableDanmaku")
+  public void setEnableDanmaku(TxplayerView view, String enableDanmaku) {
+    view.setEnableDanmaku(Boolean.valueOf(enableDanmaku));
+  }
+  @ReactProp(name = "enableFullScreen")
+  public void setEnableFullScreen(TxplayerView view, String enableFullScreen) {
+    view.setEnableFullScreen(Boolean.valueOf(enableFullScreen));
+  }
+  @ReactProp(name = "playType")
+  public void setPlayType(TxplayerView view, String playType) {
+    view.setPlayType(Integer.valueOf(playType));
+  }
+  @ReactProp(name = "playStartTime")
+  public void setPlayStartTime(TxplayerView view, String playStartTime) {
+    view.setPlayStartTime(Float.valueOf(playStartTime));
+  }
+  @ReactProp(name = "language")
+  public void setLanguage(TxplayerView view, String language) {
+    view.setLanguage(language);
   }
 }
